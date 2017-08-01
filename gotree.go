7ed12a102d8cf5dc85c76c3ab3c9e795433ee6a7
MissingL-tter <434a44@gmail.com>
2017-07-31 20:07:20 -0400

    Fixed Insert function

--- gotree.go
+++ gotree.go
@@ -1,19 +1,20 @@
 package gotree
 
-// Tree represent a binary tree structure
+// Tree represents a binary tree structure
 type Tree struct {
-	Left  *Tree
-	Value float32
-	Right *Tree
+	Parent *Tree
+	Value  float32
+	Left   *Tree
+	Right  *Tree
 }
 
-// Build takes some array of inputs and builds a binary search tree
+// Build takes some slice of float32s and builds a binary search tree
 func Build(values []float32) *Tree {
 
 	var val float32
 	val, values = values[0], values[1:]
 
-	tree := &Tree{nil, val, nil}
+	tree := &Tree{nil, val, nil, nil}
 
 	for len(values) != 0 {
 		val, values = values[0], values[1:]
@@ -25,18 +26,25 @@ func Build(values []float32) *Tree {
 }
 
 // Insert inserts a new node with passed value into the tree
+//
+// Values <= the current node's Value will branch left, while values > the current node's value will branch right
 func (tree *Tree) Insert(val float32) {
 
-	for tree.Left != nil && val <= tree.Value {
-		tree = tree.Left
+	var parent *Tree
+	for tree != nil {
+		if val <= tree.Value {
+			parent = tree
+			tree = tree.Left
+		} else {
+			parent = tree
+			tree = tree.Right
+		}
 	}
-	for tree.Right != nil && val > tree.Value {
-		tree = tree.Right
-	}
-	if val <= tree.Value {
-		tree.Left = &Tree{nil, val, nil}
+	tree = &Tree{parent, val, nil, nil}
+	if val <= parent.Value {
+		parent.Left = tree
 	} else {
-		tree.Right = &Tree{nil, val, nil}
+		parent.Right = tree
 	}
 }
 
@@ -44,8 +52,20 @@ func (tree *Tree) Insert(val float32) {
 func InOrder(tree *Tree) {
 	if tree != nil {
 		InOrder(tree.Left)
-		// fmt.Printf("%v\n", tree.Value)
+		//fmt.Printf("%v\n", tree.Value)
 		tree.Value += 0
 		InOrder(tree.Right)
 	}
+}
+
+// Search does stuff
+func (tree *Tree) Search(val float32) *Tree {
+	for tree != nil && tree.Value != val {
+		if val <= tree.Value {
+			tree = tree.Left
+		} else {
+			tree = tree.Right
+		}
+	}
+	return tree
 }
